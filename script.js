c5364b0316e0d0adf07dfcad393675e72738b9a5
Mark <markjovendonaire@gmail.com>
2025-07-18 19:54:36 +0800

    Adding function on js file  getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+function getComputerchoice(){
+    const randomNum = Math.random
+
+    if (randomNum < 0.33){
+        return "rock";
+    }
+    else if (randomNum < 0.66){
+        return "paper";
+    }
+    else return "scissors"
+}
